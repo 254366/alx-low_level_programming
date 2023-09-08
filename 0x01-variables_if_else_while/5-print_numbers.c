157b5affdc6ebca32a33911e4e8945f595f2e6da
254366 <danmaloba254@gmail.com>
2023-09-08 14:52:33 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - This is the main function of this program.
+* Return: 0 to indicate successful execution.
+*/
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+printf("%d\n", digit);
+digit++;
+}
+return (0);
+}
