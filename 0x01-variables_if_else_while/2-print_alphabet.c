ea741e9649d861701b99914d60fbe7e108ce5308
254366 <danmaloba254@gmail.com>
2023-09-08 12:39:46 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - This is the main function of this program.
+* Return: 0 to indicate successful execution.
+*/
+
+int main(void)
+{
+char x;
+
+for (x = 'a'; x <= 'y'; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
