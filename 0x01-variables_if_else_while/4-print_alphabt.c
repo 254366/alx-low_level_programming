227186fe6cb0c368b9a840b84d841313b6b517cb
254366 <danmaloba254@gmail.com>
2023-09-08 13:59:13 -0700

    4-print_alphbt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - this is the function of this program
+* Return: 0 to indicate successful execution.
+*/
+
+int main(void)
+
+{
+
+char x;
+
+for (x = 'a'; x <= 'z'; x++)
+if(x != 'q' && x != 'e')
+putchar(x);
+putchar('\n');
+return (0);
+}
