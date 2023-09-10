f47caab8ea6437731ba8b8ff89a113990fa6066c
254366 <danmaloba254@gmail.com>
2023-09-10 10:15:18 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - this the main function
+* Return: Always 0 (success)
+*/
+
+int main(void)
+
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
