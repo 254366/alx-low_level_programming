67d2ba1756d42f66664219399bc314775ac85739
254366 <danmaloba254@gmail.com>
2023-09-10 09:36:42 -0700

    echo $?

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-
 /**
-* main - A program that prints with puts function
-* Return: 0 (success)
+* main - this the main function
+* Return: Always 0 (success)
 */
+
 int main(void)
+
 {
-puts("\"programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
