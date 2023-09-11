1d0968950fcf1801cf1d916993abcb4580560ec6
254366 <danmaloba254@gmail.com>
2023-09-11 09:11:17 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,21 +6,24 @@
 int main(void)
 {
 int i, j;
-for (i = 0; i <= 99; i++)
+for (i = 0; i < 100; i++)
 {
-for (j = i; j <= 99; j++)
+for (j = i + 1; j < 100; j++)
 {
-putchar('0' + (i / 10));
-putchar('0' + (i % 10));
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
 putchar(' ');
-putchar('0' + (j / 10));
-putchar('0' + (j % 10));
-if (i != 99 || j != 99)
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+if (i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
 }
+
 }
+
 }
+putchar('\n');
 return (0);
 }
