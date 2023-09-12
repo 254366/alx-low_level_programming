ab352a51a07e203c94498e0bbe1f21133d0434af
254366 <danmaloba254@gmail.com>
2023-09-12 13:35:37 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*  prints the last digit - prints the last digit of a number
+* @n: takes number input
+* Return: last digit
+*/
+int print_last_digit(int n)
+
+{
+
+int lastdigit;
+
+if (n < 0)
+lastdigit = -1 * (n % 10);
+lastdigit = n % 10;
+_putchar(lastdigit + '0');
+return (lastdigit);
+}
