574eb4d77d01fd3ce87e898015d0e02675a029a9
254366 <danmaloba254@gmail.com>
2023-09-13 08:56:00 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,11 +1,28 @@
-# Function to generate the Fibonacci sequence up to n terms
-def generate_fibonacci(n):
-fibonacci_sequence = [1, 2]  # Starting with 1 and 2
-while len(fibonacci_sequence) < n:
-next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
-fibonacci_sequence.append(next_number)
-return fibonacci_sequence
-# Print the first 50 Fibonacci numbers separated by comma and space
-n = 50
-fibonacci_numbers = generate_fibonacci(n)
-print(', '.join(map(str, fibonacci_numbers)))
+#include <stdio.h>
+
+/**
+* main - prints the first 50 Fibonacci numbers
+* followed by a new line
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+long int i, j, k, next;
+j = 1;
+k = 2;
+for (i = 1; i <= 50; ++i)
+{
+if (i != 50)
+{
+printf("%ld, ", j);
+}
+else
+{
+printf("%ld\n", j);
+}
+next = j + k;
+j = k;
+k = next;
+}
+return (0);
+}
