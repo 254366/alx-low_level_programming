4aff16c818c7c8f3c5c8c4d97d82a5cfccf37803
254366 <danmaloba254@gmail.com>
2023-09-13 00:44:56 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*  prints the last digit - prints the last digit of a number
+* print_last_digit - prints the last digit of a number
 * @n: takes number input
 * Return: last digit
 */
